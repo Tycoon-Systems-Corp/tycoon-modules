423fa52e97582b6430b8e219c99e20b77fc9d8ec
Jesse <jessealexanderthompson@gmail.com>
2025-01-04 19:48:13 -0500

    Attempt install reqs

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ const nextConfig = {
 
 		// Example: Add resolve alias for modules folder
 		config.resolve.alias['tycoon-modules'] = path.join(__dirname, 'modules/')
+		config.resolve.alias['@tycoonsystems/tycoon-modules'] = path.join(__dirname, 'modules/')
 		config.resolve.alias['/appServer'] = path.join(__dirname, 'appServer/')
 		config.resolve.alias['/app.config'] = path.join(__dirname, 'app.config.js')
 		config.resolve.alias['/customModules'] = path.join(__dirname, 'customModules')
